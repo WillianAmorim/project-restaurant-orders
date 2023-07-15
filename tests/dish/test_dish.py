0e292b65692d701d0366fad84f6d193298bef0a6
Willian <willian.amorim1@outlook.com>
2023-07-15 00:04:01 -0300

    Finalizado requisito 2

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,32 @@
-from src.models.dish import Dish  # noqa: F401, E261, E501
+import pytest
+from src.models.dish import Dish
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
+value_error = "Dish price must be greater then zero."
+type_error = "Dish price must be float."
 
 # Req 2
 def test_dish():
-    pass
+    dish = Dish("macarronada", 20.00)
+
+    ingredient = Ingredient("bacon")
+
+    assert dish.recipe == {}
+
+    assert dish.__repr__() == f"Dish('{dish.name}', R${dish.price:.2f})"
+    assert dish == dish
+    assert dish.__hash__() == hash(dish.__repr__())
+
+    dish.add_ingredient_dependency(ingredient, 1)
+    assert dish.recipe == {ingredient: 1}
+
+    restrictions = {Restriction.ANIMAL_MEAT, Restriction.ANIMAL_DERIVED}
+    assert dish.get_restrictions() == restrictions
+
+    assert dish.get_ingredients() == {ingredient}
+
+    with pytest.raises(ValueError, match=value_error):
+        Dish(dish.name, 0)
+
+    with pytest.raises(TypeError, match=type_error):
+        Dish(dish.name, "200")
