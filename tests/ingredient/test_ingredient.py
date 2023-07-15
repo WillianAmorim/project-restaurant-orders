eb0207b17b67239e026279a66d670a3a736ed9b1
Willian <willian.amorim1@outlook.com>
2023-07-14 22:12:30 -0300

    Finalizado requisito 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,11 @@
 from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 
-# Req 1
 def test_ingredient():
-    pass
+    bacon = Ingredient("bacon")
+
+    assert bacon.name == 'bacon'
+    assert bacon == Ingredient("bacon")
+    assert repr(bacon) == f"Ingredient('{bacon.name}')"
+    assert len(bacon.restrictions) == 2
+    assert hash(bacon) == hash('bacon')
